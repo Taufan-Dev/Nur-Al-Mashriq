1a54ee26d70ec24580dba6c6161000e5f213db42
Hersya <Hersyayukione@gmail.com>
2026-02-25 02:31:15 +0700

    feat: add unique gallery images for each museum

--- src/data/locations.js
+++ src/data/locations.js
@@ -6,7 +6,13 @@ export const locations = [
         description: "Dirancang oleh arsitek legendaris I.M. Pei. Koleksinya mencakup manuskrip, keramik, tekstil, dan kaligrafi dari 3 benua.",
         imageUrl: "https://idsb.tmgrup.com.tr/ly/uploads/images/2022/05/17/206089.jpg",
         readMoreUrl: "https://mia.org.qa/",
-        panoramaUrl: "https://l13.alamy.com/360/PTT36R/museum-of-islamic-art-building-PTT36R.jpg" // Using Pannellum's free sample equirectangular image for all right now
+        panoramaUrl: "https://l13.alamy.com/360/PTT36R/museum-of-islamic-art-building-PTT36R.jpg" // Using Pannellum's free sample equirectangular image for all right now,
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/c/c4/Cropped_Museum_of_the_Romanian_Peasant_6.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/c/c4/Doha%2C_Museo_con_banderas_01.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/0/0a/Doha%2C_Museo_de_Arte_Isl%C3%A1mico_02.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/e/e4/Doha_-_Museum_of_Islamic_Art.jpg"
+        ]
     },
     {
         id: 2,
@@ -15,7 +21,13 @@ export const locations = [
         description: "Punya galeri seni Islam dengan koleksi dari abad ke-7 hingga ke-19.",
         imageUrl: "https://www.mediaoffice.abudhabi/assets/sm/upload/0n/gw/64/83/thum.jpg?k=7091c0158c",
         readMoreUrl: "https://www.louvreabudhabi.ae/",
-        panoramaUrl: "https://cloudflare1.360gigapixels.com/pano/seungsang-yoo/01932873_Abu_Simbel_Temples.jpg/equirect_crop_3_1/6.jpg"
+        panoramaUrl: "https://cloudflare1.360gigapixels.com/pano/seungsang-yoo/01932873_Abu_Simbel_Temples.jpg/equirect_crop_3_1/6.jpg",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/1/14/%27The_Tower_of_Babel%27_by_Abel_Grimmer%2C_1595.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/8/83/Gauguin_Jeunes_Lutteurs.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/5/51/IngresPedroKissSwordOfHenri4.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/6/64/La-partie-de-b%C3%A9sigue.jpg"
+        ]
     },
     {
         id: 3,
@@ -24,7 +36,13 @@ export const locations = [
         description: "Terkenal dengan koleksi karpet Utsmani dan manuskrip langka.",
         imageUrl: "https://istanbultourstudio.s3.amazonaws.com/uploads/media_content/picture/7/medium_istanbul_turkish_and_islamic_arts_interior.jpg",
         readMoreUrl: "https://muze.gen.tr/muze-detay/tiem",
-        panoramaUrl: "https://i0.wp.com/asia-archive.si.edu/wp-content/uploads/2017/11/quran-banner-TIEM_2a.jpg?resize=2555%2C1095&ssl=1"
+        panoramaUrl: "https://i0.wp.com/asia-archive.si.edu/wp-content/uploads/2017/11/quran-banner-TIEM_2a.jpg?resize=2555%2C1095&ssl=1",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/f/fc/Endowment_Charter_%28%27Waqfiyya%27%29_of_Haseki_H%C3%BCrrem_Sultan_%28TIEM_2192%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/7/7a/Figurative_architectural_piece_Artukid_dynasty_13th_century.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/3/3a/Hajj_certificate%2C_dated_602_AH_or_1205_CE%2C_Ayyubid_period%2C_Turkish_and_Islamic_Arts_Museum.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/e/e8/H%C3%BCrr%C9%99m_Sultana_aid_q%C4%B1z%C4%B1l_ba%C5%9F_b%C9%99z%C9%99yi.jpg"
+        ]
     },
     {
         id: 4,
@@ -33,7 +51,13 @@ export const locations = [
         description: "Museum seni Islam terbesar di Asia Tenggara.",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/8/88/Islamic_Arts_Museum_Malaysia_Exterior_%28May_2022%29_-_img_02.jpg",
         readMoreUrl: "https://iamm.org.my/",
-        panoramaUrl: "https://cdn.360stories.com/uploads/thumbnails/places/1572956899f4d8f702eed4ca159ab58458ff97ca03.jpeg"
+        panoramaUrl: "https://cdn.360stories.com/uploads/thumbnails/places/1572956899f4d8f702eed4ca159ab58458ff97ca03.jpeg",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/c/c0/Art_using_calligraphy_at_the_Islamic_Arts_Museum_Malaysia_%2818974180772%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/d/da/Central_illumination_of_the_Royal_Terengganu_Quran_%28Islamic_Arts_Museum_Malaysia%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/b/bc/Islamic_Arts_Museum_Malaysia_%2818976266165%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/8/88/Islamic_Arts_Museum_Malaysia_Exterior_%28May_2022%29_-_img_02.jpg"
+        ]
     },
     {
         id: 5,
@@ -42,7 +66,13 @@ export const locations = [
         description: "Memiliki koleksi seni Islam dan Timur Tengah yang sangat kaya.",
         imageUrl: "https://islamicworld.britishmuseum.org/static/hero-168ee5f1a48f6e7fc9c30dc2a35d45a2-53040.png",
         readMoreUrl: "https://www.britishmuseum.org/",
-        panoramaUrl: "https://l13.alamy.com/360/PMX8P4/british-museum-london-main-hall-PMX8P4.jpg"
+        panoramaUrl: "https://l13.alamy.com/360/PMX8P4/british-museum-london-main-hall-PMX8P4.jpg",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/9/96/1325-50_Touyl_Reliquienschrein_anagoria.JPG",
+            "https://upload.wikimedia.org/wikipedia/commons/b/b2/1891_Monet_The_four_trees_anagoria.JPG",
+            "https://upload.wikimedia.org/wikipedia/commons/6/61/20170721_Gotham_Shield_NYC_Aerials-225_medium.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/1/1a/4_Jean_Le_Noir._Miniature_from_Psalter_of_Bonne_of_Luxemburg_1348-49_Metropolitan_Museum%2C_N-Y.jpg"
+        ]
     },
     {
         id: 6,
@@ -51,7 +81,13 @@ export const locations = [
         description: "Sayap seni Islamnya sangat terkenal dengan rekonstruksi halaman Andalusia.",
         imageUrl: "https://gdb.rferl.org/c9e41c84-d829-4c2d-8223-dd789ca7e949_w1080_h608_s.jpg",
         readMoreUrl: "https://www.metmuseum.org/about-the-met/collection-areas/islamic-art",
-        panoramaUrl: "https://i.ytimg.com/vi/OTsqUNGFYlM/maxresdefault.jpg"
+        panoramaUrl: "https://i.ytimg.com/vi/OTsqUNGFYlM/maxresdefault.jpg",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/9/96/1325-50_Touyl_Reliquienschrein_anagoria.JPG",
+            "https://upload.wikimedia.org/wikipedia/commons/b/b2/1891_Monet_The_four_trees_anagoria.JPG",
+            "https://upload.wikimedia.org/wikipedia/commons/6/61/20170721_Gotham_Shield_NYC_Aerials-225_medium.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/1/1a/4_Jean_Le_Noir._Miniature_from_Psalter_of_Bonne_of_Luxemburg_1348-49_Metropolitan_Museum%2C_N-Y.jpg"
+        ]
     },
     {
         id: 7,
@@ -60,7 +96,13 @@ export const locations = [
         description: "Menampilkan seni dekoratif Islam khas Maroko.",
         imageUrl: "https://d397xw3titc834.cloudfront.net/images/original/6/ba/6ba3900e8de27602218d7f2a80c25b06.jpg",
         readMoreUrl: "https://www.visitmorocco.com/",
-        panoramaUrl: "https://gc.360-data.com/tours/KJLNuwBHb/KJLNuwBHb-HXE_bsemrIJj-scene.jpg"
+        panoramaUrl: "https://gc.360-data.com/tours/KJLNuwBHb/KJLNuwBHb-HXE_bsemrIJj-scene.jpg",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/f/fc/Bahia_Palace_large_court.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/5/59/Dar_Si_Said_1462_%28retouched%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/c/c1/Dar_Si_Said_1465.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/2/25/Dar_si_Said_DSCF1681_%28wiki%29.jpg"
+        ]
     },
     {
         id: 8,
@@ -69,7 +111,13 @@ export const locations = [
         description: "Menampilkan sains, astronomi, dan kontribusi ilmuwan Muslim.",
         imageUrl: "https://blog.visitsharjah.com/hubfs/Astrolabes-sim.jpg",
         readMoreUrl: "https://www.sharjahmuseums.ae/",
-        panoramaUrl: "https://l13.alamy.com/360/PMHG78/sharjah-museum-of-islamic-civilization-PMHG78.jpg"
+        panoramaUrl: "https://l13.alamy.com/360/PMHG78/sharjah-museum-of-islamic-civilization-PMHG78.jpg",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/c/c1/Sharjah%27s_Museum_of_Islamic_Civilisation.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/b/bf/Sharjah_Museum_of_Islamic_Civilisation_north-west_facade_%2822025908913%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/0/03/Sharjah_Museum_of_Islamic_Civilization.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Islamic_Art_at_Louvre.jpg/800px-Islamic_Art_at_Louvre.jpg"
+        ]
     },
     {
         id: 9,
@@ -78,6 +126,12 @@ export const locations = [
         description: "Memiliki koleksi seni Islam penting termasuk fasad Mshatta.",
         imageUrl: "https://universes.art/fileadmin/_processed_/3/2/csm_26-IMG_9284-A_60c0a8e013.jpg",
         readMoreUrl: "https://www.smb.museum/en/museums-institutions/pergamonmuseum/home/",
-        panoramaUrl: "https://st.depositphotos.com/1077338/3434/i/950/depositphotos_34342363-stock-photo-carpet-hall-of-islamic-art.jpg"
+        panoramaUrl: "https://st.depositphotos.com/1077338/3434/i/950/depositphotos_34342363-stock-photo-carpet-hall-of-islamic-art.jpg",
+        gallery: [
+            "https://upload.wikimedia.org/wikipedia/commons/9/9b/Aleppo_Room_-_Pergamonmuseum_-_Berlin_-_Germany_2017_%284%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/3/3e/Assarhaddon_Berlin_012008.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/9/9c/Berlin_-_Pergamonmuseum_-_Altar_01.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/6/66/Bodemuseum_-_Front.jpeg"
+        ]
     }
 ];
